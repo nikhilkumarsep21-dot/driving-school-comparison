3d1487cd743fd156e021c4a8e068a7a51cd53f57
nikhilkumarsep21-dot <nikhilkumar.sep21@gmail.com>
2025-10-11 11:55:16 +0530

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,16 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    domains: ['images.pexels.com'],
+    domains: [
+      'images.pexels.com',
+      'd3jvxfsgjxj1vz.cloudfront.net',
+      'www.gmdc.ae',
+      'blog.sothebysrealty.ae',
+      'encrypted-tbn0.gstatic.com',
+      'd2hucwwplm5rxi.cloudfront.net',
+      'www.thenationalnews.com',
+      'i.ytimg.com',
+    ],
   },
 };
 
